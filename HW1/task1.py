7732033ec4b8cb5f575b408c7520df9ab69123c6
nikita <48088106+nsuvorov17@users.noreply.github.com>
2019-10-03 17:05:33 +0300

    Add files via upload

---
+++ HW1/task1.py
@@ -0,0 +1,21 @@
+"""
+Задача 1. Уровень - легкий.
+
+Условие: Написать элементарную программу, принимающая на вход ваше имя %name% и 
+печатающая "Hello %name%!"
+
+Пример работы:
+
+Ввод:                                                Вывод:
+Sergey                                               Hello Sergey!
+
+
+Masha                                                Hello Masha!
+
+
+Victor                                               Hello Victor!
+
+"""
+
+name=input()
+print('Hello', name,'!')
